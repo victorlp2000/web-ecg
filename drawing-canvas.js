295eb86451e0b39eb14e6a39eb811dc681ca9276
Victor Liu <vliu@book.com>
2015-09-28 22:45:47 -0700

    speed up drawing

--- drawing-canvas.js
+++ drawing-canvas.js
@@ -1,18 +1,37 @@
     function DrawingCanvas(canvasElement) {
         var canvas = canvasElement;
         var context = canvasElement.getContext('2d');
-        var canvasColor = "#666666";
-        var graphColor = "#00ff00";
-        var cursorColor = "#ffffff";
+
+        var canvasColor = "rgba(255, 0, 0, 0.2)";    // background color
+        var graphColor = "#00ff00";     // graph color
+        var cursorColor = "#ffffff";    // cursor color
+
+        // to fix graph in window, value need to be normalized:
+        // factor change size, ofset change position
+        // newValue = value * factor + offset
+        var factor = 0.002;
+        var offset = 50;
+
+        // current point (x1, y1) previous point
+        // (x2, y2) new point
         var x1 = 0, x2 = 0, y1 = 0, y2 = 0;
 
-        function clearDrawArea(x, y) {
+        /*
+            clear area for new data
+            start from the current position (x1) for width
+        */
+        function clearDrawArea(width) {
             context.fillStyle = canvasColor;
-            context.fillRect(x, 0, 20, canvas.height);
-            context.fill();
+            context.fillRect(x1, 0, width + 20, canvas.height);
+            //context.fill();
+        }
 
+        /*
+            draw cursor at curent position (x1, y1)
+        */
+        function drawCursor() {
             context.beginPath();
-            context.rect(x + 5, y - 5, 5, 5);
+            context.rect(x1 + 5, y1 - 5, 5, 5);
             context.fillStyle = cursorColor;
             context.fill();
             context.strokeStyle = cursorColor;
@@ -20,30 +39,52 @@
             context.stroke();
         }
 
-        return {
-            clearCanvas : function() {
-                context.fillStyle = canvasColor;
-                context.fillRect(0, 0, canvas.width, canvas.height);
-                x1 = 0;
-                x2 = 0;
-            },
+        function normalize(value) {
+            var v = canvas.height - (value * factor + offset);
+            //console.log(value * factor);
+            return v;
+        }
 
-            drawData: function(value) {
-                x2 = x1 + 1;
-                y2 = canvas.height - (value + 25000) / 200;
-                if (x1 != -1) {
-                    clearDrawArea(x1, y2);
-                    context.beginPath();
-                    context.moveTo(x1, y1);
+        function clearCanvas() {
+            context.fillStyle = canvasColor;
+            context.fillRect(0, 0, canvas.width, canvas.height);
+            x1 = 0;
+            x2 = 0;
+        }
+
+        return {
+            clearCanvas : clearCanvas,
+
+            drawData: function(values) {
+                if (values.length <= 0)
+                    return;
+                var index = 0;
+                if (x1 == -1) {
+                    x1 = 0;
+                    y1 = normalize(values[index]);
+                    index ++;
+                    clearCanvas();
+                }
+                //clearDrawArea(values.length);
+                context.beginPath();
+                context.moveTo(x1, y1);
+
+                while (index < values.length) {
+                    x2 = x1 + 1;
+                    y2 = normalize(values[index]);
+                    index ++;
                     context.lineTo(x2, y2);
-                    context.strokeStyle = graphColor;
-                    context.stroke();
+                    x1 = x2;
+                    y1 = y2;
+                    if (x2 >= canvas.width) {
+                        x1 = -1;
+                        break;  // ignore other values,
+                                // don't have to accurate at this point
+                    }
                 }
-                x1 = x2;
-                y1 = y2;
-                if (x2 >= canvas.width) {
-                    x1 = -1;
-                }
+                context.strokeStyle = graphColor;
+                context.stroke();
+                //drawCursor();
             }
         }
     }
